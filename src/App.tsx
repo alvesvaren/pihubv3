64e65a4579d80469287d9dfa09fca870a1f7c6a6
Alve Svarén <alve@hotmail.se>
2021-11-21 21:40:43 +0100

    Add home assistant app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Card from "./components/Card";
 import { Clock, MediaPlayer, Weather } from "./components/widgets";
 import MatlistanLogo from "./img/logos/matlistan_icon.png";
 import GrafanaLogo from "./img/logos/grafana_icon.png";
+import HomeAssistantLogo from "./img/logos/home_assistant_icon.svg";
 interface AppData {
     name: string;
     imgUrl: string;
@@ -10,12 +11,13 @@ interface AppData {
 }
 
 function AppIcon(props: { app: AppData }) {
-    return <img src={props.app.imgUrl} alt={props.app.name + " logo"} />;
+    return <img className="app-icon" src={props.app.imgUrl} alt={props.app.name + " logo"} />;
 }
 
 const apps: { [id: string]: AppData } = {
     matlistan: { name: "Matlistan", imgUrl: MatlistanLogo, url: "https://www.matlistan.se/recipes" },
     grafana: { name: "Grafana", imgUrl: GrafanaLogo, url: "https://grafana.svaren.dev" },
+    homeassistant: { name: "Home Assistant", imgUrl: HomeAssistantLogo, url: "https://ha.svaren.dev" },
 };
 
 function App() {
@@ -30,6 +32,7 @@ function App() {
                 <Card className="app-bar-card">
                     <AppIcon app={apps.matlistan} />
                     <AppIcon app={apps.grafana} />
+                    <AppIcon app={apps.homeassistant} />
                 </Card>
             </div>
         </div>
