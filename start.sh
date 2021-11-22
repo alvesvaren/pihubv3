3e362460170de84eb9deb16949549ab04e7a123f
Alve Svarén <alve@hotmail.se>
2021-11-22 14:56:49 +0100

    Change port to correct number

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ sleep 5
 yarn serve &
 PIHUB_PID=$!
 sleep 5
-startx $(which chromium-browser) --window-size=801,481 --window-position=0,0 --disable-web-security --user-data-dir=$HOME/.pihub_browser --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --disable-site-isolation-trials --kiosk http://localhost:3000 -- -nocursor
+startx $(which chromium-browser) --window-size=801,481 --window-position=0,0 --disable-web-security --user-data-dir=$HOME/.pihub_browser --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --disable-site-isolation-trials --kiosk http://localhost:5000 -- -nocursor
 # Kill pihub server process after browser closes
 kill -9 $PIHUB_PID
