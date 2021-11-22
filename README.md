af6bddae096cd4d28fea3e6a7b0a28d1d70fbb64
Alve Svarén <alve@hotmail.se>
2021-11-22 14:46:43 +0100

    Add service file

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Webbsida att visa på en raspberry pi touch screen som använder sig av home ass
 ## Konfigurera:
 
 Kopiera `src/config-example.json` till `src/config.json` och ändra värdena i filen.
+
+## Använda:
+
+Länka tjänstfilen med: `sudo systemctl link pihub.service`
+Starta samt sätt på autostart för tjänsten med: `sudo systemctl enable --now pihub.service`
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sleep 5
+yarn start &
+PIHUB_PID=$!
+sleep 5
+startx $(which chromium-browser) --window-size=801,481 --window-position=0,0 --disable-web-security --user-data-dir=$HOME/.pihub_browser --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --disable-site-isolation-trials --kiosk http://localhost:3000 -- -nocursor
+# Kill pihub server process after browser closes
+kill -9 $PIHUB_PID
