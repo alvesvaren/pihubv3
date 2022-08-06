ba18bb98b700442e54840f140becf61ab9452649
Alve Svarén <alve@hotmail.se>
2022-08-06 16:11:34 +0200

    Persist scroll and close after 1h

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 import * as React from "react";
+import { useCallback } from "react";
 import { OverlayData } from "../App";
 
+const OVERLAY_TIMEOUT_MS = 1000 * 60 * 60; // 1 hour
 export const OverlayContext = React.createContext<(data: OverlayData) => void>(() => {});
 
 export default function WebOverlay(props: { data: OverlayData }) {
@@ -9,7 +11,31 @@ export default function WebOverlay(props: { data: OverlayData }) {
         data: { url, visible },
     } = props;
     const frameRef = React.useRef<HTMLIFrameElement>(null);
+    const timeoutRef = React.useRef<number>();
     const setOverlay = React.useContext(OverlayContext);
+    const timeoutWillCloseAt = React.useRef<number>();
+    window.focus();
+
+    const resetTimeout = useCallback(() => {
+        if (timeoutRef.current) {
+            window.clearTimeout(timeoutRef.current);
+        }
+        timeoutWillCloseAt.current = Date.now() + OVERLAY_TIMEOUT_MS;
+        timeoutRef.current = window.setTimeout(() => {
+            setOverlay({ url, visible: false });
+        }, OVERLAY_TIMEOUT_MS);
+    }, [setOverlay, url]);
+
+    React.useEffect(() => {
+        if (visible) resetTimeout();
+        return () => {
+            window.clearTimeout(timeoutRef.current);
+            timeoutRef.current = undefined;
+            timeoutWillCloseAt.current = undefined;
+        };
+    }, [visible, resetTimeout]);
+
+    const timeLeft = timeoutWillCloseAt.current && Math.floor((timeoutWillCloseAt.current - Date.now()) / 1000);
 
     return (
         <div id='web-overlay' className={classNames({ visible })}>
@@ -17,7 +43,9 @@ export default function WebOverlay(props: { data: OverlayData }) {
                 <button onClick={() => frameRef.current?.contentWindow?.history.back()}>{"<"}</button>
                 <button onClick={() => frameRef.current?.contentWindow?.history.forward()}>{">"}</button>
                 <span>{url}</span>
-                <button onClick={() => setOverlay({ url, visible: false })}>⨯</button>
+                <button style={{}} onClick={() => setOverlay({ url, visible: false })}>
+                    {!!(timeLeft && timeLeft < 100) && timeLeft + "s"} ⨯
+                </button>
             </nav>
             <iframe ref={frameRef} title='Web overlay' src={url} />
         </div>
